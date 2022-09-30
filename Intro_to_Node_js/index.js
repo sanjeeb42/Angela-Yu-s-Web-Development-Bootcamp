aed7cd37ba3ffa410df9fe4dd83099ecabc7f610
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-09-30 23:40:01 +0530

    Create index.js

---
+++ Intro_to_Node_js/index.js
@@ -0,0 +1,11 @@
+var superheroe=require('superheroes');
+var supervillain=require('supervillains');
+var myheroname=superheroe.random();
+var myvillain=supervillain.random();
+
+console.log(myheroname);
+console.log(myvillain);
+
+//Firstly initilize using npm to get the packgae .json file ie, npm init
+//Next install the external package --> npm install superheroes
+//Use it  
